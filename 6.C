21e2cb0b061cded4bd0ac8ab6f084682c2923026
asmaakterpoly2005-png <asmaakterpoly2005@gmail.com>
2026-08-29 20:57:59 +0600

    Add basic C program for addition of two numbers

---
+++ 6.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main ( )  
+{// means one line comment.
+ /*means multi -line comment. Remember it when you need multi-line comment*/
+ //Let's start a code🙃
+ int a =10;
+ float b=3.3333;
+ float c=a+b;
+ printf("a+b=%f",c);
+return 0;
+	
+}
