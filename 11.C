90cf2c9309b0471d52e61dd3fde55ebbc9725c6a
asmaakterpoly2005-png <asmaakterpoly2005@gmail.com>
2026-09-14 00:22:02 +0600

    Add basic arithmetic operations in 11.C

---
+++ 11.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    a=10;
+    b=2;
+    printf("%d+%d=%d\n",a,b,a+b);
+    printf("%d-%d=%d\n",a,b,a-b);
+    printf("%d*%d=%d\n",a,b,a*b);
+    printf("%d/%d=%d\n",a,b,a/b);
+    return 0;
+}
