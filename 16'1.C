c5fcdb87496fe495b3f7523621cdc6ab588fb04b
asmaakterpoly2005-png <asmaakterpoly2005@gmail.com>
2026-09-18 15:38:01 +0600

    Add program to find maximum of four integers
    Implement a program to find the maximum of four integers.

---
+++ 16'1.C
@@ -0,0 +1,43 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d;
+    scanf("%d %d %d %d",&a,&b,&c,&d);
+
+    if(a>b){
+        if(a>c){
+            if(a>d){
+                printf("Max%d\n",a);
+            }
+            else{
+                printf("Max%d\n",d);
+            }
+ 
+        } else{
+            if(c>d){
+                printf("Max%d\n",c);
+            }
+            else{
+                printf("Max%d\n",d);
+            }
+        }
+    } else{
+        if(b>c){
+            if(b>d){
+                printf("Max%d\n",b);
+            }
+            else{
+                printf("Max%d\n",d);
+            }
+         }
+        else{
+        if(c>d){
+                printf("Max%d\n",c);
+            }
+            else{
+                printf("Max%d\n",d);
+            }
+        }
+    }
+    return 0;
+}
