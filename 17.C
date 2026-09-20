302071e21f56e53f4cfa93a6933f150324101403
asmaakterpoly2005-png <asma260093@diit.edu.bd>
2026-09-20 12:32:36 +0600

    Create a C program for relational operator examples
    Added a C program demonstrating relational operators.

---
+++ 17.C
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+    int a=5;
+    int b=10;
+
+    //Relational operator(<,>,<=,>=,==,!=);output only 0/1.
+
+    printf("%d\n",a<5); // (<) it's called less than
+    printf("%d\n",b>5); // (>) it's called greather than
+    printf("%d\n",a==b); // (==) it's called equal to
+    printf("%d\n",a<=5); // (<=) it's called less than or equal to
+    printf("%d\n",b>=6); // (>=) it's called greater than or equal to 
+    printf("%d\n",a!=b); // (!=) it's called not equal 
+    
+    return 0;
+} 
+
+
+
+
+0utput:
+0
+1
+0
+1
+1
+1 
